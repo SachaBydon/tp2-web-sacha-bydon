1970c00e806a0c8883f823524b416bb066b7e6e1
SachaBydon <sacha.bydon@etu.unice.fr>
2021-10-18 12:01:40 +0200

    add todos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,8 @@ import Head from "next/head"
 
 //TODO: add eslint
 //TODO: add JSDoc
+//TODO: rework pagination (back + front URLs)
+//TODO: add #editing
 export default function MyApp({ Component, pageProps, router }: AppProps) {
   const snackbarContext = initSnackbarContext()
   const assignmentsContext = initAssignmentsContext(snackbarContext)
