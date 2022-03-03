a95c177700196fe96c75ad5cee4ec821e8dc47e9
SachaBydon <sacha.bydon@etu.unice.fr>
2022-03-03 14:41:06 +0100

    Fix tsc error

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,13 @@
 import Assignments from './Assignments'
 import AssignmentDetail from './AssignmentDetail'
 import AddAssignment from './AddAssignment'
-import AssignmentItem from './AssignmentItem'
 import LoadingFabButton from './LoadingFabButton'
 import Actions from './Actions'
-import Filters from './Filters'
 
 export {
   Assignments,
   AssignmentDetail,
   AddAssignment,
-  AssignmentItem,
   LoadingFabButton,
   Actions,
-  Filters,
 }
